bb71a4117bd5774c43c6fb6207ec795ffc1644eb
Marzena Malka <marzena.malka@gmail.com>
2023-02-04 08:27:19 +0100

    added readme

---
+++ src/App/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykanski",
+        rate: 4.4969, 
+    }, 
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.6763, 
+    }, 
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.4357, 
+    },
+    {
+        short: "CHF",
+        name: "Frank szwajcarski",
+        rate: 4.7592, 
+    },
+];
