799788873a7caa13d8eea6de2349fda65aa42b73
Marzena Malka <marzena.malka@gmail.com>
2023-03-20 20:03:02 +0100

    added theme

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        black: "#000",
+        emperor: "#555", 
+        boulder: "#777", 
+        silver: "#ccc", 
+        white: "#FFFFFF",
+        teal: "#008080", 
+    }, 
+};
