e5d39298f36f0115ef6494ae704ff7f0e681efc7
Marzena Malka <marzena.malka@gmail.com>
2023-04-02 06:57:50 +0200

    added ThemeProvider and theme

--- src/App/index.js
+++ src/App/index.js
@@ -1,33 +1,17 @@
-import { useState } from "react";
 import { Form } from './Form';
 import { Clock } from "./Clock";
-import { currencies } from './currencies';
-import { Wrapper } from "./styled";
+import { Content } from "./styled";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
 
 function App() {
-
-    const [result, setResult] = useState();
-
-    const calculateResult = (currency, amount) => {
-        const rate = currencies
-        .find(({ short }) => short === currency)
-        .rate;
-
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency
-        });
-    }
-
     return (
-        <Wrapper>
-            <Clock />
-            <Form
-            result={result}
-            calculateResult={calculateResult} 
-            />
-        </Wrapper>
+        <ThemeProvider theme={theme}>
+            <Content>
+                <Clock />
+                <Form />
+            </Content>
+        </ThemeProvider>
     );
 }
 
