55e30d36711071983fd0413a42e42efb00fbee54
Marzena Malka <marzena.malka@gmail.com>
2023-04-06 19:54:46 +0200

    updated link

--- src/App/Form/useRatesData.js
+++ src/App/Form/useRatesData.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "https://api.exchangerate.host/latest";
+const API_URL = "https://api.exchangerate.host/latest?base=PLN";
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState({
