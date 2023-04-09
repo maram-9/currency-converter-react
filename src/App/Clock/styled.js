9870b90ab2247be44a1ddfff672ad59e61c71af3
Marzena Malka <marzena.malka@gmail.com>
2023-04-09 08:52:54 +0200

    added mobilemax

--- src/App/Clock/styled.js
+++ src/App/Clock/styled.js
@@ -4,8 +4,4 @@ export const Wrapper = styled.div`
 text-align:  right;
 font-family: monospace;
 color: ${({ theme }) => theme.color.boulder};
-
-@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    padding: 5px;
-    text-align: center;
 `;
